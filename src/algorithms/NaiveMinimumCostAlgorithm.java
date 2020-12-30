280bdb498c853adea36d66c16d6337c06aaed2ee
Aycan Deniz Vit <avit16@ku.edu.tr>
2020-12-31 00:03:55 +0300

    Add Naive Minimum Cost Algorithm

--- src/algorithms/NaiveMinimumCostAlgorithm.java
+++ src/algorithms/NaiveMinimumCostAlgorithm.java
@@ -3,14 +3,22 @@ package algorithms;
 import simulator.NeighborInfo;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class NaiveMinimumCostAlgorithm extends Algorithm {
 
     @Override
     public List<NeighborInfo> selectNeighbors(String origin, String destination, String previousHop,
                                               List<NeighborInfo> neighbors) {
-        // Your code goes here.
-        return new ArrayList<>();
+        List<NeighborInfo> chosen = neighbors.stream()
+                // Make sure that we do not route back to the previous hop.
+                .filter(n -> !n.address.equals(previousHop))
+                .collect(Collectors.toList());
+        // Return the chosen nodes.
+        NeighborInfo neighborInfo = chosen.stream().min(Comparator.comparingInt(x -> x.cost)).get();
+        List<NeighborInfo> resultingList = new ArrayList<>();
+        resultingList.add(neighborInfo);
+        return resultingList;
     }
 
     @Override
